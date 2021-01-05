0b23585a135ba33db999059e64b9a3054562fb00
Theodore Stansbury <tstansbu.27@gmail.com>
2021-01-04 19:49:10 -0500

    updated config with url server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: `postgres://qifxpiblfepykg:685d95ebab20dcabcc9de4800edd6c3f6bae536a1ed0e5db6202c4d03e78dfcc@ec2-52-203-49-58.compute-1.amazonaws.com:5432/d2nc8vlisg88h0`,
+    API_ENDPOINT: `https://desolate-woodland-02809.herokuapp.com/`,
     API_KEY: process.env.REACT_APP_API_KEY
 }
